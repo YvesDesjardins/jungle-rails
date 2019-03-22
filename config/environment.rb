879b59abbee6389dda38d2796295862805282af9
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-22 13:48:24 -0700

    remove redundant require

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require_relative "./mail_settings"
 
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
