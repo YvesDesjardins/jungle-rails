9566cff34a83da4ff06d9f6d54928afa1a54b819
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-22 13:15:17 -0700

    implement form submission

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,12 @@
 class ReviewsController < ApplicationController
-  def create
+  def create 
+    @review = Review.create(allowed_params)
+    
+    redirect_to product_path(@review.product_id)
   end
+
+  private
+    def allowed_params
+      params.require(:review).permit(:rating, :description, :user_id, :product_id)
+    end
 end
